54dbd6bfbaa0e77122a130ae3c793ffec76a77c3
Tolulope <lanre.toluwa@gmail.com>
2020-11-27 16:42:23 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+import PageWrapper from './components/PageWrapper';
+import Home from './components/pages/Home';
+
+class App extends Component {
+  render(){
+    return (
+      <PageWrapper>
+        <Home />
+      </PageWrapper>
+    );
+  }
+  
+}
+
+export default App;
---
+++ src/components/pages/Home.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+class Home extends Component {
+    render(){
+        return(
+            <div>
+                <header class="masthead">
+                    <div class="container">
+                        <div class="masthead-subheading">Welcome To Our Studio!</div>
+                        <div class="masthead-heading text-uppercase">It's Nice To Meet You</div>
+                        <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#services">Tell Me More</a>
+                    </div>
+                </header>
+            </div>
+        );
+    }
+}
+
+export default Home;
