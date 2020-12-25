352982a9c8e5162f74629cfa7dffb93fecb32424
Tolulope <lanre.toluwa@gmail.com>
2020-12-25 17:30:46 +0100

    added teams

---
+++ src/components/Common/TeamMember.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+
+
+class TeamMember extends Component {
+    render(){
+        return(
+            <div className="col-lg-4">
+                <div className="team-member">
+                    <img className="mx-auto rounded-circle" src={this.props.image} alt="" />
+                    <h4>{this.props.name}</h4>
+                    <p className="text-muted">{this.props.role}</p>
+                    <Link className="btn btn-dark btn-social mx-2" to="#!"><i className="fab fa-twitter"></i></Link>
+                    <Link className="btn btn-dark btn-social mx-2" to="#!"><i className="fab fa-facebook-f"></i></Link>
+                    <Link className="btn btn-dark btn-social mx-2" to="#!"><i className="fab fa-linkedin-in"></i></Link>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default TeamMember;
